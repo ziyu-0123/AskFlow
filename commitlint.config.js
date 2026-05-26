9c0c8adf28e1064755e871ed1059c43ff6d2ec79
ziyu-0123 <ziyuliu123@qq.com>
2026-05-26 16:30:08 +0800

    chore: setup commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'ci',
+        'build',
+        'revert',
+      ],
+    ],
+    'subject-max-length': [2, 'always', 100],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+  },
+}
