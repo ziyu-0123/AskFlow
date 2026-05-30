8c918186fe090ddbf0e0263d9f2224cc9810d519
ziyu-0123 <ziyuliu123@qq.com>
2026-05-30 21:02:09 +0800

    feat: add Ant Design and refactor layouts

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,22 @@
 import type { FC } from 'react'
 import { Outlet } from 'react-router-dom'
+import { Layout } from 'antd'
+import styles from './MainLayout.module.scss'
+
+const { Header, Content, Footer } = Layout
 
 const MainLayout: FC = () => {
   return (
-    <>
-      <div>MainLayout header</div>
-      <div>
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.left}>Logo</div>
+        <div className={styles.right}>登录/用户信息</div>
+      </Header>
+      <Content className={styles.main}>
         <Outlet />
-      </div>
-      <div>MainLayout footer</div>
-    </>
+      </Content>
+      <Footer className={styles.footer}>AskFlow &copy;2026-present Created by ziyu-0123</Footer>
+    </Layout>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
+import { Button } from 'antd'
 
 const Home: FC = () => {
   const nav = useNavigate()
@@ -13,7 +14,8 @@ const Home: FC = () => {
     <>
       <p>Home</p>
       <div>
-        <button onClick={clickHandler}>登录</button>
+        <Button onClick={clickHandler}>登录</Button>
+        &nbsp;
         <Link to="/register?a=10">注册</Link>
       </div>
     </>
