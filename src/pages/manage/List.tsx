042182dd0404fc3a9701cbb91eff87a39a6947b2
ziyu-0123 <ziyuliu123@qq.com>
2026-07-13 10:10:18 +0800

    feat: implement infinite scroll pagination for questionnaire list

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,83 +1,82 @@
-// import type { FC } from 'react'
-// // import { useState, useEffect } from 'react'
-// import { useTitle } from 'ahooks'
-// // import { useSearchParams } from 'react-router-dom'
-// import { Typography, Spin } from 'antd'
-// import QuestionCard from '../../components/QuestionCard'
-// import styles from './common.module.scss'
-// import ListSearch from '../../components/ListSearch'
-// import { type QuestionListData } from '../../services/question'
-// import useLoadQuestionListData from '../../hooks/useLoadQuestionData'
-
-// const { Title } = Typography
-
-// const List: FC = () => {
-//   useTitle('AskFlow - My Questionnaires')
-
-//   const { data, loading } = useLoadQuestionListData()
-//   const { list = [], total = 0 } = (data || { list: [], total: 0 }) as QuestionListData
-
-//   // const [list, setList] = useState<QuestionData[]>([])
-//   // const [total, setTotal] = useState(0)
-
-//   // useEffect(() => {
-//   //   async function load() {
-//   //     const data = await getQuestionListService()
-//   //     const { list = [], total = 0 } = data
-//   //     setList(list)
-//   //     setTotal(total)
-//   //   }
-//   //   load()
-//   // }, [])
-
-//   // const [searchParams] = useSearchParams()
-//   // console.log('keyword', searchParams.get('keyword'))
-//   return (
-//     <>
-//       <div className={styles.header}>
-//         <div className={styles.left}>
-//           <Title level={3} style={{ marginTop: 0 }}>
-//             我的问卷
-//           </Title>
-//         </div>
-//         <div className={styles.right}>
-//           <ListSearch />
-//         </div>
-//       </div>
-//       <div className={styles.content}>
-//         {loading && (
-//           <div style={{ textAlign: 'center' }}>
-//             <Spin />
-//           </div>
-//         )}
-//         {!loading &&
-//           list.length > 0 &&
-//           list.map(q => {
-//             const { _id } = q
-//             return <QuestionCard key={_id} {...q} />
-//           })}
-//       </div>
-//       <div className={styles.footer}>loadMore... 上划加载更多...</div>
-//     </>
-//   )
-// }
-
-// export default List
-
 import type { FC } from 'react'
-import { useTitle } from 'ahooks'
+import { useState, useEffect, useRef } from 'react'
+import { useTitle, useDebounceFn, useRequest } from 'ahooks'
 import { Typography, Spin, Empty } from 'antd'
+import { useSearchParams } from 'react-router-dom'
+import { getQuestionListService } from '../../services/question'
+import { LIST_PAGE_SIZE, LIST_SEARCH_PARAM_KEY } from '../../constant'
 import QuestionCard from '../../components/QuestionCard'
 import styles from './common.module.scss'
 import ListSearch from '../../components/ListSearch'
-import useLoadQuestionListData from '../../hooks/useLoadQuestionListData'
+import type { QuestionData } from '../../services/question'
 
 const { Title } = Typography
 
 const List: FC = () => {
   useTitle('AskFlow - My Questionnaires')
 
-  const { list, loading, total } = useLoadQuestionListData()
+  const [page, setPage] = useState(1)
+  const [list, setList] = useState<QuestionData[]>([])
+  const [total, setTotal] = useState(0)
+  const haveMoreData = total > list.length
+
+  const [searchParams] = useSearchParams()
+
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  // 加载函数
+  const { run: load, loading } = useRequest(
+    async () => {
+      const data = await getQuestionListService({
+        page,
+        pageSize: LIST_PAGE_SIZE,
+        keyword: searchParams.get(LIST_SEARCH_PARAM_KEY) || '',
+      })
+      return data
+    },
+    {
+      manual: true,
+      onSuccess(result) {
+        const { list: l = [], total = 0 } = result
+        setList([...list, ...l])
+        setTotal(total)
+        setPage(page + 1)
+      },
+    }
+  )
+
+  // 尝试触发加载的函数
+  const { run: tryLoadMore } = useDebounceFn(
+    () => {
+      const elem = containerRef.current
+      if (elem === null) return
+      const domRect = elem.getBoundingClientRect()
+      if (domRect === null) return
+      const { bottom } = domRect
+      if (bottom <= document.body.clientHeight) load()
+    },
+    {
+      wait: 500,
+    }
+  )
+
+  // 1.当页面加载或者 url 参数改变时，触发 tryLoadMore
+  useEffect(() => {
+    tryLoadMore()
+  }, [searchParams])
+
+  // 2.当页面滚动时，尝试触发 tryLoadMore
+  useEffect(() => {
+    if (haveMoreData) {
+      window.addEventListener('scroll', tryLoadMore)
+    }
+
+    // 这个 cleanup 函数不会立即执行
+    // 它会在组件卸载或下次 effect 执行前才执行
+    return () => {
+      window.removeEventListener('scroll', tryLoadMore)
+    }
+  }, [searchParams, haveMoreData])
 
   return (
     <>
@@ -92,6 +91,7 @@ const List: FC = () => {
         </div>
       </div>
       <div className={styles.content}>
+        {/* <div style={{ height: '2000px' }}></div> */}
         {loading && (
           <div style={{ textAlign: 'center' }}>
             <Spin />
@@ -106,9 +106,7 @@ const List: FC = () => {
         {!loading && list.length === 0 && <Empty description="暂无数据" />}
       </div>
       <div className={styles.footer}>
-        {total > 0 && `共 ${total} 份问卷`}
-        <br />
-        上划加载更多...
+        <div ref={containerRef}>上划加载更多...</div>
       </div>
     </>
   )
