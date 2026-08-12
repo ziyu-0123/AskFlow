20aa96e47dc25b4e5d7084b529a5f72d64d07d68
ziyu-0123 <ziyuliu123@qq.com>
2026-08-12 13:55:21 +0800

    feat: display grouped components in component library

--- src/pages/question/Edit/ComponentLib.tsx
+++ src/pages/question/Edit/ComponentLib.tsx
@@ -1,18 +1,33 @@
 import type { FC } from 'react'
 import { Typography } from 'antd'
 import { componentConfGroup } from '../../../components/QuestionComponents'
+import type { ComponentConfType } from '../../../components/QuestionComponents'
+import styles from './ComponentLib.module.scss'
 
 const { Title } = Typography
+
+function genComponent(c: ComponentConfType) {
+  const { Component } = c
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.component}>
+        <Component />
+      </div>
+    </div>
+  )
+}
+
 const Lib: FC = () => {
   return (
     <>
       {componentConfGroup.map((group, index) => {
-        const { groupId, groupName } = group
+        const { groupId, groupName, components } = group
         return (
           <div key={groupId}>
             <Title level={3} style={{ marginTop: index > 0 ? '20px' : '0', fontSize: '16px' }}>
               {groupName}
             </Title>
+            <div>{components.map(c => genComponent(c))}</div>
           </div>
         )
       })}
