2b577fc28e2caa1a09549bfa31cb515d1b603ed1
ziyu-0123 <ziyuliu123@qq.com>
2026-08-17 15:50:36 +0800

    feat: implement dynamic tab switching in right panel

--- src/pages/question/Edit/RightPanel.tsx
+++ src/pages/question/Edit/RightPanel.tsx
@@ -1,9 +1,26 @@
-import type { FC } from 'react'
+import { type FC, useState, useEffect } from 'react'
 import { Tabs } from 'antd'
 import { FileTextOutlined, SettingOutlined } from '@ant-design/icons'
 import ComponentProp from './ComponentProp'
+import PageSetting from './PageSetting'
+import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
+
+// 用常量对象替代 enum（erasableSyntaxOnly 不允许 enum 语法）
+const TAB_KEYS = {
+  PROP_KEY: 'prop',
+  SETTING_KEY: 'setting',
+} as const
 
 const RightPanel: FC = () => {
+  const [activeKey, setActiveKey] = useState<'prop' | 'setting'>(TAB_KEYS.PROP_KEY)
+
+  const { selectedId } = useGetComponentInfo()
+
+  useEffect(() => {
+    if (selectedId) setActiveKey(TAB_KEYS.PROP_KEY)
+    else setActiveKey(TAB_KEYS.SETTING_KEY)
+  }, [selectedId])
+
   const tabsItems = [
     {
       key: 'prop',
@@ -23,11 +40,11 @@ const RightPanel: FC = () => {
           页面设置
         </span>
       ),
-      children: <div>页面设置</div>,
+      children: <PageSetting />,
     },
   ]
 
-  return <Tabs defaultActiveKey="prop" items={tabsItems}></Tabs>
+  return <Tabs activeKey={activeKey} items={tabsItems}></Tabs>
 }
 
 export default RightPanel
