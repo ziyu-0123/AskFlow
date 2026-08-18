a03cd6a94df930ac0b170c080b9efb3a3045816e
ziyu-0123 <ziyuliu123@qq.com>
2026-08-18 17:53:23 +0800

    feat: implement questionnaire save functionality

--- src/pages/question/Edit/EditHeader.tsx
+++ src/pages/question/Edit/EditHeader.tsx
@@ -1,12 +1,15 @@
 import { type FC, useState, type ChangeEvent } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import styles from './EditHeader.module.scss'
 import { Button, Typography, Space, Input } from 'antd'
-import { LeftOutlined, EditOutlined } from '@ant-design/icons'
+import { LeftOutlined, EditOutlined, LoadingOutlined } from '@ant-design/icons'
 import EditToolBar from './EditToolBar'
 import useGetPageInfo from '../../../hooks/useGetPageInfo'
 import { changePageTitle } from '../../../store/pageInfoReducer'
 import { useDispatch } from 'react-redux'
+import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
+import { updateQuestionService } from '../../../services/question'
+import { useRequest, useKeyPress, useDebounceEffect } from 'ahooks'
 
 const { Title } = Typography
 
@@ -41,6 +44,44 @@ const TitleElem: FC = () => {
   )
 }
 
+// 保存按钮
+const SaveButton: FC = () => {
+  const { id } = useParams()
+  const { componentList = [] } = useGetComponentInfo()
+  const pageInfo = useGetPageInfo()
+
+  const { loading, run: save } = useRequest(
+    async () => {
+      if (!id) return
+      await updateQuestionService(id, { ...pageInfo, componentList })
+    },
+    { manual: true }
+  )
+
+  // 快捷键
+  useKeyPress(['ctrl.s', 'meta.s'], (event: KeyboardEvent) => {
+    event.preventDefault()
+    if (!loading) save()
+  })
+
+  // 自动保存（不是定期保存，不是定时器）
+  useDebounceEffect(
+    () => {
+      save()
+    },
+    [componentList, pageInfo],
+    {
+      wait: 1000,
+    }
+  )
+
+  return (
+    <Button onClick={save} disabled={loading} icon={loading ? <LoadingOutlined /> : null}>
+      保存
+    </Button>
+  )
+}
+
 // 编辑器头部
 const EditHeader: FC = () => {
   const nav = useNavigate()
@@ -66,7 +107,7 @@ const EditHeader: FC = () => {
         </div>
         <div className={styles.right}>
           <Space>
-            <Button>保存</Button>
+            <SaveButton />
             <Button type="primary">发布</Button>
           </Space>
         </div>
