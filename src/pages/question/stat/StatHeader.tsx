feaed32c5f3a9b03328d1d6db83dfa5a3312de3e
ziyu-0123 <ziyuliu123@qq.com>
2026-08-22 11:48:02 +0800

    feat: add URL copy and QR code generation to stat header

--- src/pages/question/stat/StatHeader.tsx
+++ src/pages/question/stat/StatHeader.tsx
@@ -1,16 +1,53 @@
-import { type FC } from 'react'
+import { type FC, useRef } from 'react'
 import styles from './StatHeader.module.scss'
 import { useNavigate, useParams } from 'react-router-dom'
-import { Space, Button, Typography } from 'antd'
-import { LeftOutlined } from '@ant-design/icons'
+import { message, Space, Button, Typography, Input, Tooltip, Popover, type InputRef } from 'antd'
+import { LeftOutlined, CopyOutlined, QrcodeOutlined } from '@ant-design/icons'
 import useGetPageInfo from '../../../hooks/useGetPageInfo'
+import { QRCodeCanvas as QRCode } from 'qrcode.react'
 
 const { Title } = Typography
 
 const StatHeader: FC = () => {
   const nav = useNavigate()
   const { id } = useParams()
-  const { title } = useGetPageInfo()
+  const { title, isPublished } = useGetPageInfo()
+
+  // 拷贝链接
+  const urlInputRef = useRef<InputRef>(null)
+  function copy() {
+    const elem = urlInputRef.current
+    if (elem == null) return
+    elem.select() // 选中 input 的内容
+    document.execCommand('copy') // 拷贝选中内容 （富文本编辑器的操作）
+    message.success('拷贝成功')
+  }
+
+  function genLinkAndQRCodeElem() {
+    if (!isPublished) return null
+
+    // 拼接 url ，需要参考 C 端的规则
+    const url = `http://localhost:5173/question/${id}`
+
+    // 定义二维码组件
+    const QRCodeElem = (
+      <div style={{ textAlign: 'center' }}>
+        <QRCode value={url} size={150} />
+      </div>
+    )
+
+    return (
+      <Space>
+        <Input value={url} style={{ width: '300px' }} ref={urlInputRef} />
+        <Tooltip title="拷贝链接">
+          <Button icon={<CopyOutlined />} onClick={copy}></Button>
+        </Tooltip>
+        <Popover content={QRCodeElem}>
+          <Button icon={<QrcodeOutlined />}></Button>
+        </Popover>
+      </Space>
+    )
+  }
 
   return (
     <div className={styles['header-wrapper']}>
@@ -23,7 +60,7 @@ const StatHeader: FC = () => {
             <Title>{title}</Title>
           </Space>
         </div>
-        <div className={styles.main}>中间</div>
+        <div className={styles.main}>{genLinkAndQRCodeElem()}</div>
         <div className={styles.right}>
           <Button type="primary" onClick={() => nav(`/question/edit/${id}`)}>
             编辑问卷
