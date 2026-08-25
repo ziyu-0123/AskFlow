17aa0b3b4d390e89bc029de0be0c6815ac0dfdbb
ziyu-0123 <ziyuliu123@qq.com>
2026-08-25 14:16:27 +0800

    feat: update frontend URL generation logic

--- src/pages/question/stat/StatHeader.tsx
+++ src/pages/question/stat/StatHeader.tsx
@@ -26,8 +26,8 @@ const StatHeader: FC = () => {
   function genLinkAndQRCodeElem() {
     if (!isPublished) return null
 
-    // 拼接 url ，需要参考 C 端的规则
-    const url = `http://localhost:5173/question/${id}`
+    // 拼接 url ，指向 C 端(端口 3000)
+    const url = `http://localhost:3000/question/${id}`
 
     // 定义二维码组件
     const QRCodeElem = (
