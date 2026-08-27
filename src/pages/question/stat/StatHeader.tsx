40e19afe8099ddc5528944887e12acb5a704bcc5
ziyu-0123 <ziyuliu123@qq.com>
2026-08-27 12:02:52 +0800

    perf: cache links and QR codes with useMemo in stats page

--- src/pages/question/stat/StatHeader.tsx
+++ src/pages/question/stat/StatHeader.tsx
@@ -1,4 +1,4 @@
-import { type FC, useRef } from 'react'
+import { type FC, useRef, useMemo } from 'react'
 import styles from './StatHeader.module.scss'
 import { useNavigate, useParams } from 'react-router-dom'
 import { message, Space, Button, Typography, Input, Tooltip, Popover, type InputRef } from 'antd'
@@ -23,10 +23,37 @@ const StatHeader: FC = () => {
     message.success('拷贝成功')
   }
 
-  function genLinkAndQRCodeElem() {
+  // function genLinkAndQRCodeElem() {
+  //   if (!isPublished) return null
+
+  //   // 拼接 url ，指向 C 端(端口 3000)
+  //   const url = `http://localhost:3000/question/${id}`
+
+  //   // 定义二维码组件
+  //   const QRCodeElem = (
+  //     <div style={{ textAlign: 'center' }}>
+  //       <QRCode value={url} size={150} />
+  //     </div>
+  //   )
+
+  //   return (
+  //     <Space>
+  //       <Input value={url} style={{ width: '300px' }} ref={urlInputRef} />
+  //       <Tooltip title="拷贝链接">
+  //         <Button icon={<CopyOutlined />} onClick={copy}></Button>
+  //       </Tooltip>
+  //       <Popover content={QRCodeElem}>
+  //         <Button icon={<QrcodeOutlined />}></Button>
+  //       </Popover>
+  //     </Space>
+  //   )
+  // }
+
+  // 使用 useMemo 1. 依赖项是否经常变化; 2. 缓存的元素是否创建成本较高
+  const LinkAndQRCodeElem = useMemo(() => {
     if (!isPublished) return null
 
-    // 拼接 url ，指向 C 端(端口 3000)
+    // 拼接 url ，需要参考 C 端的规则
     const url = `http://localhost:3000/question/${id}`
 
     // 定义二维码组件
@@ -47,7 +74,7 @@ const StatHeader: FC = () => {
         </Popover>
       </Space>
     )
-  }
+  }, [id, isPublished])
 
   return (
     <div className={styles['header-wrapper']}>
@@ -60,7 +87,7 @@ const StatHeader: FC = () => {
             <Title>{title}</Title>
           </Space>
         </div>
-        <div className={styles.main}>{genLinkAndQRCodeElem()}</div>
+        <div className={styles.main}>{LinkAndQRCodeElem}</div>
         <div className={styles.right}>
           <Button type="primary" onClick={() => nav(`/question/edit/${id}`)}>
             编辑问卷
