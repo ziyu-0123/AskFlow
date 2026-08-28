ac08a2f38f494a156d694ae5fdb86184252a6d12
ziyu-0123 <ziyuliu123@qq.com>
2026-08-28 14:36:50 +0800

    test: add unit tests for QuestionInput and QuestionTextarea components

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom'
+
+// jsdom 不实现 ResizeObserver，antd 部分组件（如 Textarea 自动高度）依赖它
+class ResizeObserverStub {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+globalThis.ResizeObserver ??= ResizeObserverStub as unknown as typeof ResizeObserver
